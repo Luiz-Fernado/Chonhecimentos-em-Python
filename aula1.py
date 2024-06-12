3d86470f29ce3116fa736d63478dc8fcd151102d
Luiz-Fernado <luizfernandoaraujodeandrade7@gmail.com>
2024-06-12 16:57:00 -0300

    aula1.py
    teste

--- aula1.py
+++ aula1.py
@@ -1,1 +1,12 @@
 print("Olá")
+
+def controlevelocidade():
+    velocidade = 50
+    contator = 0
+    while contatortentativas < 10 and <= velocidade <= 100:
+        print(f"velocidade atual: {velocidade} km")
+        imput=("desejar aumentar (A) ou diminuir (D) a velocidade")
+        if opcao == "A":
+            velocidade += 10
+        elif opcao == "D":
+            velocidade -= 10
