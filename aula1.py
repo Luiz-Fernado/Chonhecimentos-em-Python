5b05451495252d8090384b5989df178a309539ee
Luiz-Fernado <luizfernandoaraujodeandrade7@gmail.com>
2024-06-17 14:19:29 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,12 +1,23 @@
 print("Olá")
 
-def controlevelocidade():
+def contadordevelocidade():
     velocidade = 50
-    contator = 0
-    while contatortentativas < 10 and <= velocidade <= 100:
-        print(f"velocidade atual: {velocidade} km")
-        imput=("desejar aumentar (A) ou diminuir (D) a velocidade")
-        if opcao == "A":
+    contador = 0
+    print("chamou")
+    while contador < 10 and velocidade >=0 and velocidade <= 100:
+        print(f"velocidade atual= {velocidade} km")
+        op = input("deseja aumentar (A) ou diminuir (D) a velocidade?").strip().upper()
+        if op == "A":
             velocidade += 10
-        elif opcao == "D":
-            velocidade -= 10
+        elif op == "D":
+             velocidade -= 10
+        else:
+            print("escolha invalida,tente novamente.")
+            continue
+        contador += 1
+
+        if velocidade <= 0 or velocidade >= 100:
+            break
+        print(f"operacao finalizada,velocidade final: {velocidade} km")
+
+contadordevelocidade()
